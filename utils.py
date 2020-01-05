aae0224779d152168dcf522f7c911b22cde6b2ba
Kyryll Kuchynskyi <kuchinskii1999@gmail.com>
2020-01-05 03:21:05 +0200

    add doc tests and .travis.yml file

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ def load(filename):
 def preproccess_images(x, y, split):
     """Convert to binary format image, extracts only 0 and 1
     classes from the train data set.
+
     """
     # convert images from range {0,1,...,255} to range {0,1}
     x = (x > 127).astype(np.uint8)
@@ -41,18 +42,41 @@ def preproccess_images(x, y, split):
     return x_train, y_train, x_test, y_test
 
 
-def generate_aposterior_probabilities(n_samples,n_classes):
+def generate_aposterior_probabilities(n_samples, n_classes, seed=None):
     """Generate a matrix with shape (n_samples,n_classes), where
     sum along axis=1 = 1. p(k|xz)
+    >>> generate_aposterior_probabilities(1, 2, 1)
+    array([[0.05575326, 0.94424674]])
+    >>> generate_aposterior_probabilities(2, 2, 1)
+    array([[0.05575326, 0.94424674],
+           [0.24672846, 0.75327154]])
+    >>> generate_aposterior_probabilities(2, 2, 5)
+    array([[0.56393887, 0.43606113],
+           [0.70279118, 0.29720882]])
+    >>> generate_aposterior_probabilities(2, 3, 10)
+    array([[0.17764609, 0.63712789, 0.18522602],
+           [0.51900452, 0.17797888, 0.30301659]])
+    >>> generate_aposterior_probabilities(2, 4, 7)
+    array([[0.49790161, 0.14222429, 0.06260201, 0.29727209],
+           [0.43703704, 0.09325216, 0.13921867, 0.33049214]])
     """
+    np.random.seed(seed)
     rand_matrix = np.random.randint(low=0, high=5000,size=(n_samples, n_classes))
-    sum_along_row = np.repeat(rand_matrix.sum(axis=1).reshape((-1, 1)), repeats=2, axis=1)
+    sum_along_row = np.repeat(rand_matrix.sum(axis=1).reshape((-1, 1)), repeats=rand_matrix.shape[1], axis=1)
     prob_matrix = rand_matrix/sum_along_row
     return prob_matrix
 
 
 def aprior_probabilities(probability_histogram):
     """calculate p(k)
+    >>> aprior_probabilities(np.array([[0.05575326, 0.94424674], [0.24672846, 0.75327154]]))
+    array([0.15124086, 0.84875914])
+    >>> aprior_probabilities(np.array([[0.24672846, 0.75327154], [0.24672846, 0.75327154]]))
+    array([0.24672846, 0.75327154])
+    >>> aprior_probabilities(np.array([[0.56393887, 0.43606113], [0.70279118, 0.29720882]]))
+    array([0.63336503, 0.36663497])
+    >>> aprior_probabilities(np.array([[0.17764609, 0.63712789, 0.18522602], [0.51900452, 0.17797888, 0.30301659]]))
+    array([0.3483253 , 0.40755339, 0.2441213 ])
     """
     return probability_histogram.mean(axis=0)
 
@@ -67,11 +91,11 @@ def calculate_parameters(x, aposterior_probs):
     """
     znam = aposterior_probs.sum(axis=0)
     znam = np.expand_dims(znam, axis=1)
-    znam = np.repeat(znam, repeats=28*28, axis=1).reshape((2, 28, 28))
+    znam = np.repeat(znam, repeats=28*28, axis=1).reshape((2, x.shape[1], x.shape[2]))
     probs = np.expand_dims(aposterior_probs.T, axis=2)
-    probs = np.repeat(probs, repeats=28*28, axis=2).reshape((2, x.shape[0], 28, 28))
+    probs = np.repeat(probs, repeats=28*28, axis=2).reshape((2, x.shape[0], x.shape[1], x.shape[2]))
     x = np.expand_dims(x, axis=0)
-    x = np.repeat(x, repeats=2, axis=0)
+    x = np.repeat(x, repeats=aposterior_probs.shape[1], axis=0)
     return np.multiply(x, probs).sum(axis=1)/znam
 
 
@@ -82,6 +106,7 @@ def calculate_conditional_probs(x, parameters):
 
     Returns : 
     a matrix of shape (8292,2)
+
     """
     parameters = np.expand_dims(parameters, axis=0)
     parameters = np.repeat(parameters, repeats=x.shape[0], axis=0)
@@ -100,6 +125,12 @@ def calculate_aposterior(conditions, aprior_probs):
     aprior_probs - (2,)
     Returns :
     a matrix with shape (8292,2)
+    >>> calculate_aposterior(np.array([[0.05575326, 0.94424674], [0.24672846, 0.75327154]]),np.array([0.32, 0.87]))
+    array([[0.02125615, 0.97874385],
+           [0.10752169, 0.89247831]])
+    >>> calculate_aposterior(np.array([[0.56393887, 0.43606113], [0.70279118, 0.29720882]]), np.array([0.05575326, 0.94424674]))
+    array([[0.07094334, 0.92905666],
+           [0.12251496, 0.87748504]])
     """
     znam = conditions[:, 0]*aprior_probs[0] + conditions[:, 1]*aprior_probs[1]
     aposterior0 = (conditions[:, 0]*aprior_probs[0])/znam
@@ -108,6 +139,10 @@ def calculate_aposterior(conditions, aprior_probs):
 
 
 def evaluate(x, y, params):
+    """
+    >>> evaluate(np.array([[0.56393887, 0.43606113]]), np.array([0, 0]), np.array([[0.05575326, 0.94424674], [0.24672846, 0.75327154]]))
+    2.0
+    """
     pred = calculate_conditional_probs(x, params).argmax(axis=1)
     error = (pred - y.flatten()).sum()/len(pred)
     if error < 0:
